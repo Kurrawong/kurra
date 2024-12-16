e14823367d1ccbc6654bd0d01e65d338b0779797
Nicholas Car <nick@kurrawong.ai>
2024-12-16 23:19:18 +1000

    generalised upload function; query function; tests

---
+++ kurrawong/utils.py
@@ -0,0 +1,10 @@
+def _guess_rdf_data_format(rdf: str):
+    rdf = rdf.strip()
+    if rdf.startswith("PREFIX") or rdf.startswith("@prefix"):
+        return "text/turtle"
+    elif rdf.startswith("{") or rdf.startswith("["):
+        return "application/ld+json"
+    elif rdf.startswith("<?xml") or rdf.startswith("<rdf"):
+        return "application/rdf+xml"
+    else:
+        return "application/n-triples"
