15376254ad5611bd9537990836843ff1da0342c4
Nicholas Car <nick@kurrawong.ai>
2025-01-03 17:39:52 +1000

    improved func signatures

--- kurra/utils.py
+++ kurra/utils.py
@@ -3,7 +3,7 @@ from typing import Union
 from rdflib import Graph
 
 
-def guess_format_from_data(rdf: str):
+def guess_format_from_data(rdf: str) -> str | None:
     if rdf is not None:
         rdf = rdf.strip()
         if rdf.startswith("PREFIX") or rdf.startswith("@prefix"):
@@ -20,7 +20,7 @@ def guess_format_from_data(rdf: str):
         return None
 
 
-def load_graph(file_or_str_or_graph: Union[Path, str, Graph]):
+def load_graph(file_or_str_or_graph: Union[Path, str, Graph]) -> Graph:
     """Presents an RDFLib Graph object from a parses source or a wrapper SPARQL Endpoint"""
     if isinstance(file_or_str_or_graph, Path):
         return Graph().parse(str(file_or_str_or_graph))
