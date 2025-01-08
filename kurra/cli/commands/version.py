5bd5a0aed75372c734df7d4606e3a3e0d6321e16
Nicholas Car <nick@kurrawong.ai>
2025-01-08 13:21:48 +1000

    remove --version option

--- kurra/cli/commands/version.py
+++ kurra/cli/commands/version.py
@@ -1,16 +1,14 @@
 import typer
-from typing_extensions import Annotated
 
-from kurra.cli import app as main_app
 from kurra import __version__
 from kurra.cli.console import console
 
 app = typer.Typer()
 
 
-@app.command(name="version", help="Show the version of the kurra CLI app.")
+@app.command(name="version", help="Show the version of the kurra app.")
 def version_command():
-    console.print(f"kurra version {__version__}")
+    console.print(f"{__version__}")
 
 
 def version_callback(value: bool):
@@ -19,13 +17,3 @@ def version_callback(value: bool):
 
         version.version_command()
         raise typer.Exit()
-
-
-@main_app.callback(invoke_without_command=True)
-def main(
-    version: Annotated[
-        bool, typer.Option("--version", callback=version_callback, is_eager=True)
-    ] = False,
-):
-    """Main callback for the CLI app."""
-    pass
