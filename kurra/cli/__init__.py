2e23d6b96c6573364966194998003b9d62191a4b
Edmond Chuc <edmond@kurrawong.ai>
2025-01-08 15:08:38 +1000

    feat: move fuseki related commands under a fuseki subcommand

--- kurra/cli/__init__.py
+++ kurra/cli/__init__.py
@@ -3,6 +3,6 @@ from kurra.cli.commands.format import app as format_app
 from kurra.cli.commands.version import app as version_app
 from kurra.cli.commands.fuseki.app import app as fuseki_app
 
-app.add_typer(fuseki_app)
+app.add_typer(fuseki_app, name="fuseki")
 app.add_typer(format_app)
 app.add_typer(version_app)
