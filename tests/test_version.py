cb9c4e3bb703470aef288edc813d639a1c28a5b5
Nicholas Car <nick@kurrawong.ai>
2025-01-08 14:34:02 +1000

    fix version test

--- tests/test_version.py
+++ tests/test_version.py
@@ -9,10 +9,4 @@ runner = CliRunner()
 def test_version_command():
     result = runner.invoke(app, ["version"])
     assert result.exit_code == 0
-    assert result.output == f"kurra version {__version__}\n"
-
-
-def test_version_callback():
-    result = runner.invoke(app, ["--version"])
-    assert result.exit_code == 0
-    assert result.output == f"kurra version {__version__}\n"
+    assert result.output == f"{__version__}\n"
