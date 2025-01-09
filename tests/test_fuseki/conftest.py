7b2fa9023930867dfc7acdc85a118af8e0ed5170
Edmond Chuc <edmond@kurrawong.ai>
2025-01-09 15:29:08 +1000

    fix: find and replace error

--- tests/test_fuseki/conftest.py
+++ tests/test_fuseki/conftest.py
@@ -4,17 +4,17 @@ import pytest
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.waiting_utils import wait_for_logs
 
-FUSEKI_IMAGE = "ghcr.io/kurrawong/db-geosparql:git-main-e642d849"
+FUSEKI_IMAGE = "ghcr.io/kurrawong/fuseki-geosparql:git-main-e642d849"
 
 
 @pytest.fixture(scope="module")
 def fuseki_container(request: pytest.FixtureRequest):
     container = DockerContainer(FUSEKI_IMAGE)
     container.with_volume_mapping(
-        str(Path(__file__).parent / "shiro.ini"), "/db/shiro.ini"
+        str(Path(__file__).parent / "shiro.ini"), "/fuseki/shiro.ini"
     )
     container.with_volume_mapping(
-        str(Path(__file__).parent / "config.ttl"), "/db/config.ttl"
+        str(Path(__file__).parent / "config.ttl"), "/fuseki/config.ttl"
     )
     container.with_exposed_ports(3030)
     container.start()
